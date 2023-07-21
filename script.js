4ce3061ee642af94c9ee2388e8b68fc0ad0ee1ac
Priyanka-K-A <priyankaashokan0607@gmail.com>
2023-07-21 21:07:19 +0530

    init

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const degree=6;
-const hr = document.querySelector('#hr');
-const min = document.querySelector('#min');
-const sec = document.querySelector('#sec');
+const hr = document.getElementById('hr');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
 
 setInterval(() => {
 
@@ -13,4 +13,54 @@ setInterval(() => {
     hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
     sec.style.transform = `rotateZ(${ss}deg)`;
+
 })
+
+const dateDayWeek = document.getElementById('date-day-week'),
+      dateMonth = document.getElementById('date-month'),
+      dateDay = document.getElementById('date-day'),
+      dateYear = document.getElementById('date-year'),
+      textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm')
+
+const clockText = () => {
+    let date = new Date()
+    let dayWeek = date.getDay(),
+    month = date.getMonth(),
+    day = date.getDate(),
+    year = date.getFullYear(),
+    hh = date.getHours(),
+    mm = date.getMinutes(),
+    ampm
+
+    let daysWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    dateDayWeek.innerHTML = `${daysWeek[dayWeek]}`
+    dateMonth.innerHTML = `${months[month]}`
+    dateDay.innerHTML = `${day}, `
+    dateYear.innerHTML = year
+
+    if(hh >= 12){
+        // hh = hh - 12
+        ampm = 'PM'
+     } else{
+        ampm = 'AM'
+     }
+  
+     textAmPm.innerHTML = ampm
+  
+     if(hh == 0){hh = 12}
+  
+     if(hh < 10){hh = `0${hh}`}
+  
+     textHour.innerHTML = `${hh}:`
+  
+     if(mm < 10){mm = `0${mm}`}
+  
+     textMinutes.innerHTML = mm
+  }
+  setInterval(clockText, 1000)
+
+
